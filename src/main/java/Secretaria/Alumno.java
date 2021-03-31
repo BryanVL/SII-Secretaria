1f84ee605e19894d9f65ba4702d07778a9b8d179
Bryan <velicka.b@uma.es>
2021-03-31 10:30:57 +0200

    Tutorial

---
+++ src/main/java/Secretaria/Alumno.java
@@ -0,0 +1,30 @@
+package Secretaria;
+
+import java.io.Serializable;
+import java.lang.Long;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Alumno
+ *
+ */
+@Entity
+
+public class Alumno implements Serializable {
+
+	@Id
+	private Long dni;
+	private static final long serialVersionUID = 1L;
+
+	public Alumno() {
+		super();
+	}   
+	public Long getDni() {
+		return this.dni;
+	}
+
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+   
+}
