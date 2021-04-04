3a2bb223fa7198726bb9b25687fa0b89348a24b6
daviddltorre <davidelfutbolista2014@uma.es>
2021-04-04 13:12:43 +0200

    Main añadido

---
+++ src/main/java/Secretaria/Main.java
@@ -0,0 +1,19 @@
+package Secretaria;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 EntityManagerFactory emf = Persistence.createEntityManagerFactory("Secretaria");
+		 EntityManager em = emf.createEntityManager();
+		 
+		 em.close();
+		 emf.close();
+		 System.out.println("Tablas creadas");
+	}
+
+}
