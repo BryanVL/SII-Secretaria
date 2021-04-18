3d04d5ccac4c554936c440a52b3fef47496c297e
Bryan <velicka.b@uma.es>
2021-04-18 14:23:31 +0200

    Añadida dependencia (javax) y añadido configuración de build (maven-ejb)

--- src/main/java/ejb/AlumnoImpl.java
+++ src/main/java/ejb/AlumnoImpl.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.util.Random;
 import java.util.logging.Logger;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
