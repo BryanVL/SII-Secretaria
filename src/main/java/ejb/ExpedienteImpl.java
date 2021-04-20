ca4cdfaf2df17b8dcb22ddfd8c830a950145a745
daviddltorre <davidelfutbolista2014@uma.es>
2021-04-20 02:06:41 +0200

    Importar Expediente hecho(Hay que comprobar si está bien)

--- src/main/java/ejb/ExpedienteImpl.java
+++ src/main/java/ejb/ExpedienteImpl.java
@@ -44,7 +44,7 @@ public class ExpedienteImpl implements InterfazImportar,InterfazExpediente{
 	public void Importar(String dir) throws ImportarException {
 		// TODO Auto-generated method stub
 		if(dir.substring(dir.length()-4).equals("xlsx")) {
-			
+			//Para el archivo csv de 'alumnos' 
 		       File f = new File(dir);
 		       InputStream inp = null;
 			try {
@@ -67,26 +67,54 @@ public class ExpedienteImpl implements InterfazImportar,InterfazExpediente{
 		       
 		       Sheet sh = wb.getSheetAt(0);
 		       int iRow = 0;
-			Row row = sh.getRow(iRow); //En qué fila empezar ya dependerá también de si tenemos, por ejemplo, el título de cada columna en la primera fila
+		       Row row = sh.getRow(iRow); //En qué fila empezar ya dependerá también de si tenemos, por ejemplo, el título de cada columna en la primera fila
+		       int n=1;
 		       while(row!=null) 
 		       {
-		           Cell cell = row.getCell(1);  
-		           String value = cell.getStringCellValue();
-		           System.out.println("Valor de la celda es " + value);
+		    	  
+		    	   if(n>=5) {
+		           Cell cell = row.getCell(4);  
+		           String notaMedia = cell.getStringCellValue();
+		           cell = row.getCell(17);  
+		           String creditosSuperados = cell.getStringCellValue();
+		           cell = row.getCell(18);  
+		           String nExpediente = cell.getStringCellValue();
+	               Expediente e = new Expediente();
+		               
+	               e.setNota_media_provisional(Float.parseFloat(notaMedia));
+	               e.setCreditos_superados(Float.parseFloat(creditosSuperados));
+	               e.setNum_expediente(Integer.parseInt(nExpediente));
+	               e.setActivo(true);
+	            	
+	               em.persist(e);
+		    	   }
+		           n++;
 		           iRow++;  
 		           row = sh.getRow(iRow);
 		       }
 		}else if(dir.substring(dir.length()-3).equals("csv")){
+			//Para el archivo xlsx de 'Datos alumnadoFAKE' sin título
 			BufferedReader reader;
 			try {
 				reader = Files.newBufferedReader(Paths.get(dir));
-				CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);			
+				CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);		
+				int n=1;
 	            for (CSVRecord csvRecord : csvParser) {
-	            	String name = csvRecord.get(0);
-	                String email = csvRecord.get(1);
-	                String phone = csvRecord.get(2);
-	                String country = csvRecord.get(3);
-				    
+	            	
+	            	if(n>=5) {
+	            		
+	            	String nExpediente = csvRecord.get(4);
+	                String notaMedia = csvRecord.get(17);
+	                String creditosSuperados = csvRecord.get(18);
+	                Expediente e = new Expediente();
+	               
+	                e.setNota_media_provisional(Float.parseFloat(notaMedia));
+	                e.setCreditos_superados(Float.parseFloat(creditosSuperados));
+	                e.setNum_expediente(Integer.parseInt(nExpediente));
+	                e.setActivo(true);
+	            	em.persist(e);
+	            	}
+	            	n++;
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
