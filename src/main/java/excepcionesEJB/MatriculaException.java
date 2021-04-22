31926861f7915319da9829536bba90ef5be2c8af
Noel <nowel13@uma.es>
2021-04-22 12:51:35 -0700

    Importar Matricula(Faltan cosas), Visualizar Matricula e interfaz/excepcion de Matricula.

---
+++ src/main/java/excepcionesEJB/MatriculaException.java
@@ -0,0 +1,13 @@
+package excepcionesEJB;
+
+public class MatriculaException extends Exception{
+	
+	public MatriculaException() {
+		
+	}
+	
+	public MatriculaException(String msg) {
+		super(msg);
+	}
+
+}
