793760fd64f00bb23b5418dcdc9a3a46b2ba656d
daviddltorre <davidelfutbolista2014@uma.es>
2021-04-24 22:23:14 +0200

    RF7 Y 11 DE ASIGNATURA Y OPTATIVA

--- src/test/java/testEJB/AsignaturaPrueba.java
+++ src/test/java/testEJB/AsignaturaPrueba.java
@@ -1,8 +1,5 @@
 package testEJB;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import java.util.logging.Logger;
 
 import javax.naming.NamingException;
@@ -11,32 +8,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 import es.uma.informatica.sii.anotaciones.Requisitos;
-import excepcionesEJB.ImportarException;
-import excepcionesEJB.TitulacionException;
+import interfacesEJB.InterfazAsignatura;
 import interfacesEJB.InterfazImportar;
 import interfacesEJB.InterfazOptativa;
-import jpa.Titulacion;
 
-
-public class OptativaPrueba {
+public class AsignaturaPrueba {
 
 	private static final Logger LOG = Logger.getLogger(OptativaPrueba.class.getCanonicalName());
 
-	private static final String TITULACION_EJB = "java:global/classes/OptativaImpl!ejb.OptativaImpl";	
+	private static final String TITULACION_EJB = "java:global/classes/AsignaturaImpl!ejb.AsignaturaImpl";	
 	
 	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "SecretariaTest";
 	
 	
 	
 	private InterfazImportar interfazImportar;
-	private InterfazOptativa interfazOptativa;
+	private InterfazAsignatura interfazAsignatura;
 	
 	
 	
 	@Before
 	public void setup() throws NamingException  {
 		interfazImportar = (InterfazImportar) SuiteTest.ctx.lookup(TITULACION_EJB);
-		interfazOptativa = (InterfazOptativa) SuiteTest.ctx.lookup(TITULACION_EJB);
+		interfazAsignatura = (InterfazAsignatura) SuiteTest.ctx.lookup(TITULACION_EJB);
 		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
 	}
 	
@@ -50,9 +44,9 @@ public class OptativaPrueba {
 	@Requisitos({"RF11"})
 	public void testVisualizarOptativa() {
 		
+		
 
 		
 	}
-	
 	
 }
