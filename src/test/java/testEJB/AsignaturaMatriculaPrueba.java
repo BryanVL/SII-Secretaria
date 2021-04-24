9c244c9d31dec42ab23e4801d3498bd9fd29907b
daviddltorre <davidelfutbolista2014@uma.es>
2021-04-24 22:08:05 +0200

    Tests de Asignaturas_Matriculas Actualizado

---
+++ src/test/java/testEJB/AsignaturaMatriculaPrueba.java
@@ -0,0 +1,57 @@
+package testEJB;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.logging.Logger;
+
+import javax.naming.NamingException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import interfacesEJB.InterfazAsignaturas_Matricula;
+import interfacesEJB.InterfazMatricula;
+
+public class AsignaturaMatriculaPrueba {
+	
+	private static final Logger LOG = Logger.getLogger(MatriculaPrueba.class.getCanonicalName());
+	private static final String GRUPO_EJB = "java:global/classes/Asignaturas_MatriculaImpl!ejb.Asignaturas_MatriculaImpl";	
+	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "SecretariaTest";
+	
+	
+	private InterfazAsignaturas_Matricula interfazAsignaturas_Matricula; 
+
+		
+	
+	@Before
+	public void setup() throws NamingException  {
+		
+		interfazAsignaturas_Matricula = (InterfazAsignaturas_Matricula)  SuiteTest.ctx.lookup(GRUPO_EJB);
+		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
+	}
+	
+	@Test
+	public void testVisualizarAsignaturas_Matricula() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void testFiltrarPorAsignatura() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void testFiltrarPorGrupo() {
+		assertEquals(1,1);
+	}
+	
+	@Test
+	public void testFiltrarPorAsignaturas_Matricula() {
+		assertEquals(1,1);
+	}
+	
+	
+	
+
+	
+}
