362771a320cc4fcb1515bede49f37788fb1217c2
Noel <nowel13@uma.es>
2021-05-17 15:32:35 -0700

    Probando a importar desde excel

--- Secretaria-ejb/src/main/java/ejb/AlumnoImpl.java
+++ Secretaria-ejb/src/main/java/ejb/AlumnoImpl.java
@@ -6,17 +6,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
-import java.util.logging.Logger;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.persistence.Column;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -24,19 +20,16 @@ import javax.persistence.TypedQuery;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 
 import excepcionesEJB.AlumnoException;
 import excepcionesEJB.ImportarException;
 import interfacesEJB.InterfazAlumno;
 import interfacesEJB.InterfazImportar;
 import jpa.Alumno;
-import jpa.Expediente;
 
 
 
@@ -54,86 +47,111 @@ public class AlumnoImpl implements InterfazImportar,InterfazAlumno{
 		if(dir.endsWith("xlsx")) {
 			//Para el archivo xlsx de 'Datos alumnadoFAKE'
 			
-			File f = new File(dir);
-			InputStream inp = null;
+			FileInputStream inp = null;
 			
 			try {
-				inp = new FileInputStream(f);
+				 inp = new FileInputStream(new File(dir));
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-		    Workbook wb = null;
+			HSSFWorkbook workbook = null;
 			
-		    try {
-				wb = WorkbookFactory.create(inp);
-			} catch (EncryptedDocumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
+			try {
+				workbook = new HSSFWorkbook(inp);
+			} catch(IOException e) {
 				e.printStackTrace();
 			}
-		      
-			Sheet sh = wb.getSheetAt(0);
-			int iRow = 0;
-		    
-			Row row = sh.getRow(iRow); //En qué fila empezar ya dependerá también de si tenemos, por ejemplo, el título de cada columna en la primera fila
-		    int n=1; 
-		    
-		    while(row!=null) 
-		    {
-		    	
-		    	if(n>=5) {
-		    		  
-		    		Cell cell = row.getCell(1);  
-		    		String dni = cell.getStringCellValue();
-		    		cell = row.getCell(2);  
-		    		String nombre = cell.getStringCellValue();
-		    		cell = row.getCell(3);  
-		    		String apellido1 = cell.getStringCellValue();
-		    		cell = row.getCell(4);  
-		    		String apellido2 = cell.getStringCellValue();
-		    		cell = row.getCell(7);  
-		    		String email_i = cell.getStringCellValue();
-		    		cell = row.getCell(8);  
-		    		String email_p = cell.getStringCellValue();
-		    		cell = row.getCell(9);  
-		    		String telefono = cell.getStringCellValue();
-		    		cell = row.getCell(10);  
-		    		String movil = cell.getStringCellValue();
-		    		cell = row.getCell(11);  
-		    		String direccion = cell.getStringCellValue();
-		    		cell = row.getCell(12);  
-		    		String localidad = cell.getStringCellValue();
-		    		cell = row.getCell(13);  
-		    		String provincia = cell.getStringCellValue();
-		    		cell = row.getCell(14);  
-		    		String cp = cell.getStringCellValue();
-		    		
-		    		Alumno a = new Alumno();
-		    		a.setDNI(dni);
-		    		a.setNombre(nombre);
-		    		a.setApellido1(apellido1);
-		    		a.setApellido2(apellido2);
-		    		a.setEmail_institucional(email_i);
-		    		a.setEmail_personal(email_p);
-		    		a.setTelefono( Integer.parseInt(telefono.replaceAll(" ","")) );
-		    		a.setMovil(Integer.parseInt(movil.replaceAll(" ", "")) );
-		    		a.setDireccion(direccion);
-		    		a.setLocalidad(localidad);
-		    		a.setProvincia(provincia);
-		    		a.setCP( Integer.parseInt(cp) );
-		    		
-		    		em.persist(a);
-		    	}
-		    	
-		        n++;
-		        iRow++;  
-		        row = sh.getRow(iRow);
-		    }
-		       
+			
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			
+			Iterator<Row> filaIterator = sheet.iterator();
+			
+			int contF = 0;
+			Row fila;
+			
+			while(filaIterator.hasNext()) {
+				
+				fila = filaIterator.next();
+				
+				if(contF > 3) {
+					
+					Iterator<Cell> celdaIterator = fila.cellIterator();
+					
+					int contC = 1;
+					Cell celda;
+					
+					if(celdaIterator.hasNext()) {
+						
+						String dni="";
+						String nombre="";
+						String apellido1="";
+						String apellido2="";
+						String email_i="";
+						String email_p="";
+						String telefono="";
+						String movil="";
+						String direccion="";
+						String localidad="";
+						String provincia="";
+						String cp="";
+					
+						while(celdaIterator.hasNext()) {
+							celda = celdaIterator.next();
+							
+							switch(contC) {
+								case(1):
+									dni = celda.getStringCellValue();
+								case(2):
+									nombre = celda.getStringCellValue();
+								case(3):
+									apellido1 = celda.getStringCellValue();
+								case(4):
+									apellido2 = celda.getStringCellValue();
+								case(7):
+									email_i = celda.getStringCellValue();
+								case(8):
+									email_p = celda.getStringCellValue();
+								case(9):
+									telefono = celda.getStringCellValue();
+								case(10):
+									movil = celda.getStringCellValue();
+								case(11):
+									direccion = celda.getStringCellValue();
+								case(12):
+									localidad = celda.getStringCellValue();
+								case(13):
+									provincia = celda.getStringCellValue();
+								case(14):
+									cp = celda.getStringCellValue();
+								default:
+							}
+							contC++;
+						}	
+						
+						Alumno a = new Alumno();
+			    		a.setDNI(dni);
+			    		a.setNombre(nombre);
+			    		a.setApellido1(apellido1);
+			    		a.setApellido2(apellido2);
+			    		a.setEmail_institucional(email_i);
+			    		a.setEmail_personal(email_p);
+			    		a.setTelefono( Integer.parseInt(telefono.replaceAll(" ","")) );
+			    		a.setMovil(Integer.parseInt(movil.replaceAll(" ", "")) );
+			    		a.setDireccion(direccion);
+			    		a.setLocalidad(localidad);
+			    		a.setProvincia(provincia);
+			    		a.setCP(Integer.parseInt(cp));
+						
+			    		em.persist(a);
+					}
+					
+				}else {
+					contF++;
+				}
+				
+			}
+			
 		} else if (dir.endsWith("csv")){
 			 //Para el archivo csv de 'alumnos' 
 			BufferedReader reader;
@@ -179,7 +197,6 @@ public class AlumnoImpl implements InterfazImportar,InterfazAlumno{
 				}
 	            
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
             
@@ -192,7 +209,6 @@ public class AlumnoImpl implements InterfazImportar,InterfazAlumno{
 	
 	@Override
 	public void validarAcceso(Alumno a) throws AlumnoException {
-		// TODO Auto-generated method stub
     	Alumno alumno = em.find(Alumno.class, a.getID());
     	
     	if(alumno==null) {
@@ -204,7 +220,6 @@ public class AlumnoImpl implements InterfazImportar,InterfazAlumno{
 	
 	@Override
 	public Alumno VisualizarAlumno(String dni) throws AlumnoException {
-		// TODO Auto-generated method stub
 		
 		TypedQuery <Alumno> query = em.createQuery("Select a from Alumno a where a.DNI = :fdni", Alumno.class);
     	query.setParameter("fdni", dni);
