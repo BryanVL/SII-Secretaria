7125ec54755d5430228edf24113f2006245ff4a3
Noel <nowel13@uma.es>
2021-06-06 11:39:41 -0700

    Quitar test crearUsuario

--- Secretaria-ejb/src/test/java/testEJB/UsuarioPrueba.java
+++ Secretaria-ejb/src/test/java/testEJB/UsuarioPrueba.java
@@ -10,32 +10,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 import es.uma.informatica.sii.anotaciones.Requisitos;
-import excepcionesEJB.AlumnoException;
 import excepcionesEJB.UsuarioException;
-import interfacesEJB.InterfazAlumno;
 import interfacesEJB.InterfazUsuario;
-import jpa.Alumno;
 import jpa.Usuario;
 
 public class UsuarioPrueba {
 
 	private static final Logger LOG = Logger.getLogger(AlumnoPrueba.class.getCanonicalName());
 
-	private static final String ALUMNO_EJB = "java:global/classes/AlumnoImpl!ejb.AlumnoImpl";
 	private static final String USUARIO_EJB = "java:global/classes/UsuarioImpl!ejb.UsuarioImpl";
 	
 	private static final String UNIDAD_PERSITENCIA_PRUEBAS = "SecretariaTest";
 	
-	
-	
-	private InterfazAlumno interfazAlumno;
 	private InterfazUsuario interfazUsuario;
 	
 	
 	
 	@Before
 	public void setup() throws NamingException  {
-		interfazAlumno  = (InterfazAlumno) SuiteTest.ctx.lookup(ALUMNO_EJB);
 		interfazUsuario = (InterfazUsuario) SuiteTest.ctx.lookup(USUARIO_EJB);
 		
 		BaseDatos.inicializaBaseDatos(UNIDAD_PERSITENCIA_PRUEBAS);
@@ -57,20 +49,4 @@ public class UsuarioPrueba {
 		}
 	}
 	
-	@Test
-	@Requisitos({"RF"})
-	public void testCrearUsuario() {
-		try {
-			
-			Alumno alumno = interfazAlumno.VisualizarAlumno("125681D");
-			Usuario usuario = new Usuario();
-			interfazUsuario.crearUsuario("125680C", "Nowel13", "pepepe13");
-			
-			
-		} catch(AlumnoException e) {
-			e.printStackTrace();
-		} catch(UsuarioException e) {
-			e.printStackTrace();
-		}
-	}
 }
